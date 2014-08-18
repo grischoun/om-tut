47e0ac3335083be8a0d4e570661c26b20ae7a8db
Chrislain Razafimahefa <razafima@gmail.com>
2014-08-18 10:09:16 +0200

    initial commit

---
+++ src/om_tut/core.cljs
@@ -0,0 +1,17 @@
+(ns om-tut.core
+  (:require [om.core :as om :include-macros true]
+            [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+(def app-state (atom {:text "Welcome to OM Mr!!"}))
+
+(om/root
+  (fn [app owner]
+    (reify om/IRender
+      (render [_]
+        (dom/h1 nil (:text app)))))
+  app-state
+  {:target (. js/document (getElementById "app"))})
+
+(swap! app-state assoc :text "Do it live!")
